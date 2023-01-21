60775789bf1b523ddc89f9e18a367d125b9c7048
yuki528 <Kiplimomeli44@gmail.com>
2023-01-21 02:01:21 -0800

    first commit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,57 +1,82 @@
 #include "sort.h"
+
 /**
- * quick_sort - function that sorts an array
- * of integers in ascending order using the
- * quick sort algorithm
- *
- * @array: input arrray
- * @size: size of the array
- * Return: no return
+ * quick_sort - partitions sorts by half
+ * @array: the array
+ * @size: size of length
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
+	if (size < 2)
+		return;
+
 	_qsort(array, 0, size - 1, size);
 }
-/**
- * _qsort - auxiliar function for the
- * quick_sort function
- * @a: input arrray
- * @low: index for the first element
- * @high: index for the last element
- * @size: size of the array
- * Return: no return
- */
-void _qsort(int *a, int low, int high, int size)
-{
-	int p, w, i;
-	int tmp;
 
-	if (low < high)
-	{
-		p = high;
-		w = low;
-		for (i = low; i < high; i++)
-		{
-			if (a[i] < a[p])
-			{
-				if (i != w)
-				{
-					tmp = a[i];
-					a[i] = a[w];
-					a[w] = tmp;
-					print_array(a, size);
-				}
-				w++;
-			}
-		}
-		if (w != p && a[w] != a[p])
-		{
-			tmp = a[w];
-			a[w] = a[p];
-			a[p] = tmp;
-			print_array(a, size);
-		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
-	}
+/**
+ * _qsort - recursively calls itself to move left and right
+ * @array: the array
+ * @left: left pointer
+ * @right: right pointer
+ * @size: size of length
+ * Return: void
+ */
+void _qsort(int *array, int left, int right, size_t size)
+{
+	int index = partition(array, left, right, size);
+
+	if (left >= right)
+		return;
+
+	_qsort(array, left, index - 1, size);
+	_qsort(array, index + 1, right, size);
 }
+
+/**
+ * partition - partition sorts by pivot point
+ * @array: the array
+ * @left: left pointer
+ * @right: right pointer
+ * @size: size of array length
+ * Return: void
+ */
+size_t partition(int *array, int left, int right, size_t size)
+{
+	int pivot = array[right];
+	int i = left, j = 0;
+
+	for (j = left; j < right; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j && array[i] != array[j])
+			{
+				swap(array, i, j);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != right && array[i] != array[j])
+	{
+		swap(array, i, right);
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * swap - swaps two given indexes
+ * @array: the array
+ * @left: left pointer
+ * @right: right pointer
+ * Return: void
+ */
+void swap(int *array, int left, int right)
+{
+	array[left] += array[right];
+	array[right] = array[left] - array[right];
+	array[left] -= array[right];
+}
+
